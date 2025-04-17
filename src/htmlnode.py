ac4962b08a777e994bd3dddeb62f67d23cac5fb1
Martin Holmes <holmes-a9@macbookpro.home>
2025-04-17 21:16:16 +0100

    added parentnode with unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,7 +14,7 @@ class HTMLNode:
         props_html = ""
         for prop in self.props:
             props_html += f' {prop}="{self.props[prop]}"'
-        return props_html.lstrip()
+        return props_html
 
     def __repr__(self):
         return f"HTMLNode('{self.tag}', {self.value}, children: {self.children}, {self.props})"
@@ -22,9 +22,49 @@ class HTMLNode:
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
-        super().__init__(tag, value, None, props=props)
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.props is None:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
-        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
+        if self.value is None:
+            raise ValueError("invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+#
+# Create another child class of HTMLNode called ParentNode. Its constructor should differ from HTMLNode in that:
+# The tag and children arguments are not optional
+# It doesn't take a value argument
+# props is optional
+# (It's the exact opposite of the LeafNode class)
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, children=children, props=props)
+
+    def __repr__(self):
+        return f"ParentNode('{self.tag}', {self.children}, {self.props})"
+
+    # Add a .to_html method.
+    # If the object doesn't have a tag, raise a ValueError.
+    # If children is a missing value, raise a ValueError with a different message.
+    # Otherwise, return a string representing the HTML tag of the node and its children.
+    # This should be a recursive method (each recursion being called on a nested child node).
+    # I iterated over all the children and called to_html on each,
+    # concatenating the results and injecting them between the opening and closing tags of the parent.
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("invalid HTML: no nested html available")
+
+        children_concat = ""
+        for node in self.children:
+            children_concat += node.to_html()
+        return f"<{self.tag}<{self.props_to_html()}>{children_concat}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -13,7 +13,7 @@ class TestHTMLNode(unittest.TestCase):
         )
         self.assertEqual(
             html_node_1.props_to_html(),
-            'href="https://www.google.com" target="_blank"',
+            ' href="https://www.google.com" target="_blank"',
         )
 
         self.assertEqual(
@@ -28,9 +28,56 @@ class TestLeafNode(unittest.TestCase):
         leaf_node_2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual(leaf_node_1.to_html(), "<p>This is a paragraph</p>")
         self.assertEqual(
+            leaf_node_1.__repr__(), "LeafNode(p, This is a paragraph, None)"
+        )
+        self.assertEqual(
             leaf_node_2.to_html(), '<a href="https://www.google.com">Click me!</a>'
         )
 
+    def test_raises(self):
+        leaf_node_1_raises = LeafNode("p", None)
 
+        with self.assertRaises(ValueError):
+            leaf_node_1_raises.to_html()
+
+
+class TestParentNode(unittest.TestCase):
+    def test_eq(self):
+        parent_node_1 = ParentNode("p", "<p>hello me</p>")
+        self.assertEqual(
+            parent_node_1.__repr__(), "ParentNode('p', <p>hello me</p>, None)"
+        )
+
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+
+# TODO:
+# Add more tests for html parentnode with nested leafnodes. Especially with props
 if __name__ == "__main__":
     unittest.main()
